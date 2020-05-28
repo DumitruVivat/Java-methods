771d29d3935622d04964a5e9a7600e3f23002c33
DumitruVivat <59474795+DumitruVivat@users.noreply.github.com>
2020-05-28 19:57:18 +0300

    Add files via upload

---
+++ BasicMethods2.java
@@ -0,0 +1,19 @@
+package myproject;
+
+public class BasicMethods2 {
+	
+	static void Hello(String x) {
+		System.out.println( x );
+	}
+
+	public static void main(String[] args) {
+		
+		Hello("Hello!");
+		Hello("Bye");
+		Hello("Salut!");
+		Hello("Pa-pa");
+
+		
+	}
+
+}
---
+++ Methods.java
@@ -0,0 +1,27 @@
+package myproject;
+
+public class Methods {
+	
+	static void Hello( ) {
+		System.out.println( "Hello!" );
+	}
+	static void Salut( ) {
+		System.out.println( "Salut!" );
+	}
+	static void Bye( ) {
+		System.out.println( "Bye" );
+	}
+	static void Papa( ) {
+		System.out.println( "Pa-pa" );
+	}
+
+	public static void main(String[] args) {
+		
+		Hello();
+		Salut();
+		Bye();
+		Papa();
+
+	}
+
+}
