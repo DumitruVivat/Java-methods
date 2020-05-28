e2d9f4601c24457f908e0236ccaa97284906f572
DumitruVivat <59474795+DumitruVivat@users.noreply.github.com>
2020-05-28 20:03:59 +0300

    Add files via upload

---
+++ For_Continue.java
@@ -0,0 +1,16 @@
+package myproject;
+
+public class For_Continue {
+
+	public static void main(String[] args) {
+		//continue
+		for(int m = 1; m <= 10; m++) {
+			if( m == 3) {
+				continue;
+			}
+			System.out.println( m );
+		}
+
+	}
+
+}
